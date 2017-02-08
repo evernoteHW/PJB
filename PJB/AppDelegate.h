ff17f1310ce9c03d57705b5914b1febdbcbda5ca
evernoteHW <apple@macbook-pro-2.local>
2017-02-08 09:51:25 +0800

    Initial Commit

---
+++ PJB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PJB
+//
+//  Created by apple on 17/2/8.
+//  Copyright © 2017年 WeiHu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
